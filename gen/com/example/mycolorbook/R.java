19d5d91918eec1581bf705f97c54a0b2dc147115
jdanielsmx <jdsandoval@gmail.com>
2014-08-17 03:09:06 -0500

    add drawing support

--- gen/com/example/mycolorbook/R.java
+++ gen/com/example/mycolorbook/R.java
@@ -1111,9 +1111,13 @@ containing a value of this type.
         public static final int brush=0x7f020057;
         public static final int eraser=0x7f020058;
         public static final int ic_launcher=0x7f020059;
-        public static final int new_pic=0x7f02005a;
-        public static final int paint=0x7f02005b;
-        public static final int save=0x7f02005c;
+        public static final int large=0x7f02005a;
+        public static final int medium=0x7f02005b;
+        public static final int new_pic=0x7f02005c;
+        public static final int paint=0x7f02005d;
+        public static final int paint_pressed=0x7f02005e;
+        public static final int save=0x7f02005f;
+        public static final int small=0x7f020060;
     }
     public static final class id {
         public static final int action_bar=0x7f05001f;
@@ -1127,7 +1131,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050019;
         public static final int action_menu_presenter=0x7f05001a;
         public static final int action_mode_close_button=0x7f050027;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f05004e;
         public static final int activity_chooser_view_content=0x7f050028;
         public static final int always=0x7f05000e;
         public static final int any=0x7f050000;
@@ -1137,13 +1141,16 @@ containing a value of this type.
         public static final int btnTakeImage=0x7f050041;
         public static final int checkbox=0x7f050030;
         public static final int collapseActionView=0x7f050010;
-        public static final int colorbookcameraview_activity_java_surface_view=0x7f050044;
+        public static final int colorbookcameraview_activity_java_surface_view=0x7f05004a;
         public static final int default_activity_button=0x7f05002b;
         public static final int dialog=0x7f050011;
         public static final int disableHome=0x7f05000b;
+        public static final int draw_btn=0x7f050045;
+        public static final int drawing=0x7f050048;
         public static final int dropdown=0x7f050012;
         public static final int edit_query=0x7f050033;
         public static final int end=0x7f050016;
+        public static final int erase_btn=0x7f050046;
         public static final int expand_activities_button=0x7f050029;
         public static final int expanded_menu=0x7f05002f;
         public static final int front=0x7f050002;
@@ -1152,16 +1159,21 @@ containing a value of this type.
         public static final int icon=0x7f05002d;
         public static final int ifRoom=0x7f05000d;
         public static final int image=0x7f05002a;
-        public static final int imageView1=0x7f050043;
+        public static final int large_brush=0x7f05004d;
         public static final int listMode=0x7f050004;
         public static final int list_item=0x7f05002c;
+        public static final int medium_brush=0x7f05004c;
         public static final int middle=0x7f050015;
         public static final int never=0x7f05000c;
+        public static final int new_btn=0x7f050044;
         public static final int none=0x7f050013;
         public static final int normal=0x7f050003;
+        public static final int paint_colors=0x7f050049;
         public static final int progress_circular=0x7f05001b;
         public static final int progress_horizontal=0x7f05001c;
         public static final int radio=0x7f050032;
+        public static final int relativeLayout1=0x7f050043;
+        public static final int save_btn=0x7f050047;
         public static final int search_badge=0x7f050035;
         public static final int search_bar=0x7f050034;
         public static final int search_button=0x7f050036;
@@ -1176,6 +1188,7 @@ containing a value of this type.
         public static final int showCustom=0x7f05000a;
         public static final int showHome=0x7f050007;
         public static final int showTitle=0x7f050009;
+        public static final int small_brush=0x7f05004b;
         public static final int split_action_bar=0x7f050021;
         public static final int submit_area=0x7f05003c;
         public static final int tabMode=0x7f050005;
@@ -1243,7 +1256,8 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int activity_paint_image=0x7f030019;
         public static final int activity_take_image=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int brush_chooser=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1296,9 +1310,12 @@ containing a value of this type.
         public static final int brush=0x7f0a0015;
         public static final int erase=0x7f0a0016;
         public static final int hello_world=0x7f0a000e;
+        public static final int lrg=0x7f0a001b;
+        public static final int med=0x7f0a001a;
         public static final int paint=0x7f0a0018;
         public static final int paint_image=0x7f0a0010;
         public static final int save=0x7f0a0017;
+        public static final int sml=0x7f0a0019;
         public static final int start_new=0x7f0a0014;
         public static final int take_image=0x7f0a0011;
         public static final int title_activity_paint_image=0x7f0a0012;
--- src/com/example/mycolorbook/PaintImageActivity.java
+++ src/com/example/mycolorbook/PaintImageActivity.java
@@ -8,7 +8,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
-
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -16,10 +15,28 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import java.util.UUID;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class PaintImageActivity extends ActionBarActivity {
 
+public class PaintImageActivity extends ActionBarActivity implements OnClickListener {
+
+	//custom drawing view
+	private DrawingView drawView;
+	//buttons
+	private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn;
+	//sizes
+	private float smallBrush, mediumBrush, largeBrush;
 	private String strFilename;
 	
 	@Override
@@ -28,10 +45,42 @@ public class PaintImageActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_paint_image);
 		
 		Intent intent = getIntent();
-		strFilename = intent.getStringExtra("FILENAME");
-		LoadImage();
+
+		//get drawing view
+		drawView = (DrawingView)findViewById(R.id.drawing);
+
+		//get the palette and first color button
+		LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+		currPaint = (ImageButton)paintLayout.getChildAt(0);
+		currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+		//sizes from dimensions
+		smallBrush = getResources().getInteger(R.integer.small_size);
+		mediumBrush = getResources().getInteger(R.integer.medium_size);
+		largeBrush = getResources().getInteger(R.integer.large_size);
+
+		//draw button
+		drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+		drawBtn.setOnClickListener(this);
+
+		//set initial size
+		drawView.setBrushSize(mediumBrush);
+
+		//erase button
+		eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
+		eraseBtn.setOnClickListener(this);
+
+		//new button
+		newBtn = (ImageButton)findViewById(R.id.new_btn);
+		newBtn.setOnClickListener(this);
+
+		//save button
+		saveBtn = (ImageButton)findViewById(R.id.save_btn);
+		saveBtn.setOnClickListener(this);
+		
+		//LoadImage();
 	}
-	
+	/*
 	private void LoadImage()
 	{
 		/*
@@ -44,7 +93,7 @@ public class PaintImageActivity extends ActionBarActivity {
 		    myImage.setImageBitmap(myBitmap);
 		}
 		*/
-		File file = new File(strFilename);	   	    
+/*		File file = new File(strFilename);	   	    
 	    if(file.exists())
 	    {
 
@@ -58,7 +107,7 @@ public class PaintImageActivity extends ActionBarActivity {
 
 		    Core.subtract(invertcolormatrix, mIntermediateMat, mIntermediateMat);
 	    	*/
-	    	Mat mInv = m;//mIntermediateMat; //invertcolormatrix;
+	/*    	Mat mInv = m;//mIntermediateMat; //invertcolormatrix;
 	    	Bitmap bm = Bitmap.createBitmap(mInv.cols(), mInv.rows(),Bitmap.Config.ARGB_8888);
 	        Utils.matToBitmap(mInv, bm);
 		    ImageView myImage = (ImageView) findViewById(R.id.imageView1);
@@ -68,7 +117,7 @@ public class PaintImageActivity extends ActionBarActivity {
 		    
 	    }
 	}
-
+*/
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.addSubMenu("Color Effect");
@@ -87,5 +136,155 @@ public class PaintImageActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	//user clicked paint
+	public void paintClicked(View view){
+		//use chosen color
+
+		//set erase false
+		drawView.setErase(false);
+		drawView.setBrushSize(drawView.getLastBrushSize());
+
+		if(view!=currPaint){
+			ImageButton imgView = (ImageButton)view;
+			String color = view.getTag().toString();
+			drawView.setColor(color);
+			//update ui
+			imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+			currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+			currPaint=(ImageButton)view;
+		}
+	}
+	
+	@Override
+	public void onClick(View view){
+
+		if(view.getId()==R.id.draw_btn){
+			//draw button clicked
+			final Dialog brushDialog = new Dialog(this);
+			brushDialog.setTitle("Brush size:");
+			brushDialog.setContentView(R.layout.brush_chooser);
+			//listen for clicks on size buttons
+			ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+			smallBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(false);
+					drawView.setBrushSize(smallBrush);
+					drawView.setLastBrushSize(smallBrush);
+					brushDialog.dismiss();
+				}
+			});
+			ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+			mediumBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(false);
+					drawView.setBrushSize(mediumBrush);
+					drawView.setLastBrushSize(mediumBrush);
+					brushDialog.dismiss();
+				}
+			});
+			ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+			largeBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(false);
+					drawView.setBrushSize(largeBrush);
+					drawView.setLastBrushSize(largeBrush);
+					brushDialog.dismiss();
+				}
+			});
+			//show and wait for user interaction
+			brushDialog.show();
+		}
+		else if(view.getId()==R.id.erase_btn){
+			//switch to erase - choose size
+			final Dialog brushDialog = new Dialog(this);
+			brushDialog.setTitle("Eraser size:");
+			brushDialog.setContentView(R.layout.brush_chooser);
+			//size buttons
+			ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+			smallBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(true);
+					drawView.setBrushSize(smallBrush);
+					brushDialog.dismiss();
+				}
+			});
+			ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+			mediumBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(true);
+					drawView.setBrushSize(mediumBrush);
+					brushDialog.dismiss();
+				}
+			});
+			ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+			largeBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					drawView.setErase(true);
+					drawView.setBrushSize(largeBrush);
+					brushDialog.dismiss();
+				}
+			});
+			brushDialog.show();
+		}
+		else if(view.getId()==R.id.new_btn){
+			//new button
+			AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+			newDialog.setTitle("New drawing");
+			newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
+			newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					drawView.startNew();
+					dialog.dismiss();
+				}
+			});
+			newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					dialog.cancel();
+				}
+			});
+			newDialog.show();
+		}
+		else if(view.getId()==R.id.save_btn){
+			//save drawing
+			AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+			saveDialog.setTitle("Save drawing");
+			saveDialog.setMessage("Save drawing to device Gallery?");
+			saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					//save drawing
+					drawView.setDrawingCacheEnabled(true);
+					//attempt to save
+					String imgSaved = MediaStore.Images.Media.insertImage(
+							getContentResolver(), drawView.getDrawingCache(),
+							UUID.randomUUID().toString()+".png", "drawing");
+					//feedback
+					if(imgSaved!=null){
+						Toast savedToast = Toast.makeText(getApplicationContext(), 
+								"Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+						savedToast.show();
+					}
+					else{
+						Toast unsavedToast = Toast.makeText(getApplicationContext(), 
+								"Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+						unsavedToast.show();
+					}
+					drawView.destroyDrawingCache();
+				}
+			});
+			saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which){
+					dialog.cancel();
+				}
+			});
+			saveDialog.show();
+		}
 	}
 }
